25aaa82520d8b7e713f96127d89a07b9909eec78
JPantana <joshpantana@me.com>
2019-03-06 19:49:40 -0600

    printToDom and createProjectCards functions are working

--- main.js
+++ main.js
@@ -1,1 +1,89 @@
-console.log('testing testing');
+
+
+const projects = [
+    {
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux",
+    },
+    { 
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux",
+
+    },
+    {
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux",
+
+    },
+    {
+        title: "Uncool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: false,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux",
+
+    },
+    {
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux",
+
+    },
+];
+
+
+// printToDom function 
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+
+// createProjectCards function
+const createProjectCards = () => {
+    let domString = '';
+    for (let i = 0; i < projects.length; i++) {
+        if (projects[i].available === true) {
+        domString += `<div class="card">`;
+        domString +=   `<h2>Title: ${projects[i].title}</h2>`;
+        domString +=   `<img src="${projects[i].screenshot}">`;
+        domString +=   `<p>Description: ${projects[i].description}</p>`;
+        domString +=   `<p>Technolgies Used: ${projects[i].technologiesUsed}</p>`;
+        domString +=   `<p>Available: ${projects[i].available}</p>`;
+        domString +=   `<p><a href="${projects[i].url}">Link</a></p>`;
+        domString +=   `<p>${projects[i].githubUrl}</p>`;
+        domString += `</div>`;
+      } else {
+          console.log('damn');
+      }
+    printToDom('projectsPage', domString);
+    }
+};
+
+
+const init = () => {
+    createProjectCards();
+};
+
+init();
