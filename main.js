9fe797abc2147d0b7ca880c2074959f762a0cc0d
JPantana <joshpantana@me.com>
2019-03-26 16:08:47 -0500

    wip

--- main.js
+++ main.js
@@ -4,25 +4,6 @@ const printToDom = (divId, textToPrint) => {
     selectedDiv.innerHTML = textToPrint;
 };
 
-
-
-// const imgJS = () => {
-//     const new Image();
-//     for (let i = 0; i < projects.screenshot.length; i++) {
-//     document.getElementsByClassName('projectsPage')
-//     }
-// };
-
-
-
-// const imgJS = new Image();
-// imgJS.src = 'image.png';
-// imgJS.onclick = function() {
-//     window.location.href = 'https://www.dropbox.com/s/a2g9ax3y74054ca/JP%20PRESS%20LOWREZ%20b%26w%201.jpg?dl=0';
-// };
-// document.getElementById('projectsPage').projects.screenshot(imgJS);
-// projects.screenshot.innerHTML = "   https://www.dropbox.com/s/a2g9ax3y74054ca/JP%20PRESS%20LOWREZ%20b%26w%201.jpg?dl=0"
-
 const projects = [
     {
         title: "Cool Project", 
@@ -75,10 +56,6 @@ const projects = [
     },
 ];
 
-
-
-
-
 // createProjectCards function
 const createProjectCards = () => {
     let domString = '';
@@ -100,9 +77,108 @@ const createProjectCards = () => {
     }
 };
 
+const hideProjects = () => {
+    let domString = '';
+    printToDom('mainDiv', domString);
+};
+
+
+const bioDivBuilder = (e) => {
+    console.log(e);
+    e.preventDefault();
+    let domString = '';
+        domString += `<div class="mainDiv topPage" id="bioPage">`;
+        domString += `    <div class="">`;
+        domString += `      <h1 id="aboutH1"><span id="josh2">Josh</span>Pantana</h1>`;
+        domString += `     <h3></em>software developer</em></h3>`;
+        domString += `     <p id="bio">Hello! I’m Josh Pantana. I’m a full-stack software developer residing in Nashville, TN. I have a serious passion for designing an intuitive and inspiring UI/UX experience. Nearly 6 weeks into my journey of becoming a full stack web developer, I'm in love with the process of creating highly functional, aesthetically engaging sites. Lets make the web a more beautiful place together!</p>`;
+        domString += `    </div>`;
+        domString += `  </div>`;
+        printToDom('mainDiv', domString);
+        buttonEvents();
+};
+
+const techDivBuilder = (e) => {
+    e.preventDefault();
+    let domString = '';
+        domString += `        <div id="triangleBackground">`
+        domString += `            <div class="fullPage" id="technologiesPage">`
+        domString += `                <h1 class="" id="techProf">Technologies</h1>`
+        domString += `            </div>`
+        domString += `                <ul class="tech-container container"> `
+        domString += `                    <li>`
+        domString += `                        <div class="techImgDiv">`
+        domString += `                            <img data-wow-delay=".1s" class="techImg wow bounceIn logo-width" 
+                                                    src="imgs/html5.png" style="visibility: visible; animation-delay: 0.1s; 
+                                                    animation-name: pulse;">`
+        domString += `                        <h3 class="techH3">HTML5</h3>`
+        domString += `                        </div>`
+        domString += `                    </li>`
+        domString += `                    <li></li>`
+        domString += `                        <div class="techImgDiv">`
+        domString += `                            <img data-wow-delay=".1s" class="techImg wow bounceIn logo-width" 
+                                                    src="imgs/css3.png" style="visibility: visible; animation-delay: 0.1s; 
+                                                    animation-name: pulse;">`
+        domString += `                        <h3 class="techH3">CSS3</h3>`
+        domString += `                        </div>`
+        domString += `                    </li>`
+        domString += `                    <li>`
+        domString += `                        <div class="techImgDiv">`
+        domString += `                            <img data-wow-delay=".1s" class="techImg wow bounceIn logo-width" 
+                                                    src="imgs/JS6_Logo.png" style="visibility: visible; animation-delay: 0.1s; 
+                                                    animation-name: pulse;">`
+        domString += `                        <h3 class="techH3">JavaScript</h3>`
+        domString += `                        </div>`
+        domString += `                    </li>`
+        domString += `                    <li>`
+        domString += `                        <div class="techImgDiv">`
+        domString += `                            <img data-wow-delay=".1s" class="techImg wow bounceIn logo-width" 
+                                                    src="imgs/bootstrap.png" style="visibility: visible; animation-delay: 0.1s; 
+                                                    animation-name: pulse;">`
+        domString += `                        <h3 class="techH3">Bootstrap</h3>`
+        domString += `                        </div>`
+        domString += `                    </li>`
+        domString += `                    <li>`
+        domString += `                        <div class="techImgDiv">`
+        domString += `                            <img data-wow-delay=".1s" class="techImg wow bounceIn logo-width" 
+                                                    src="imgs/xcode-logo.png" style="visibility: visible; animation-delay: 0.1s; 
+                                                    animation-name: pulse;">`
+        domString += `                        <h3 class="techH3">xcode</h3>`
+        domString += `                        </div>`
+        domString += `                    </li>`
+        domString += `                </ul>`
+        domString += `            </div>`
+        domString += `         </div>`
+        printToDom('mainDiv', domString);
+        buttonEvents();
+};
+
+// const projectsDomBuilder = (e) => {
+//     e.preventDefault();
+//     let domstring = '';
+//         domstring += `<div class="projects-page clearfix">`;
+//         domstring += `  <div id="projectsDiv">`;
+//         domstring += `      <h1 id="projectsH1" class="projectsH" id="">Projects</h1>`;
+//         domstring += `  </div>`;
+//         domstring += `  <div class="jsPage" id="projectsPage"></div>`;
+//         domstring += `</div>`;
+//         console.log(domstring);
+//     createProjectCards();
+//     buttonEvents();
+// };
+
+const buttonEvents = () => {
+    document.getElementById('bioLink').addEventListener('click', bioDivBuilder);
+    document.getElementById('techLink').addEventListener('click', techDivBuilder);
+    // document.getElementById('projectLink').addEventListener('click', projectsDomBuilder);
+
+};
+
 
 const init = () => {
-    createProjectCards();
+    buttonEvents();
 };
+
 init();
+
 
