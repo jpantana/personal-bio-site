e2e58eb64a21985a53b8e083b8ebcb1ccdc732fb
JPantana <joshpantana@me.com>
2019-06-22 10:28:11 -0500

    wip

--- src/javascripts/components/projects/projects.js
+++ src/javascripts/components/projects/projects.js
@@ -12,14 +12,15 @@ const createProjectCards = () => {
       for (let i = 0; i < projects.length; i += 1) {
         if (projects[i].available === true) {
           domString += `
-            <div class="card">
+            <div class="myCard">
             <h2 class="titleH2">${projects[i].title}</h2>
             <img class="projectImages" src="${projects[i].screenshot}"/>
             <p class="jsDescrip"><strong>Description:</strong> ${projects[i].description}</p>
             <p class="jsTech"><strong>Technolgies Used:</strong></br> ${projects[i].technologiesUsed}</p>
-            <p class="jsAvail"><strong>Available:</strong> ${projects[i].available}</p>
-            <p><a class="jsLinks" href="${projects[i].url}">Link</a></p>
-            <p><a class="jsLinks" href="${projects[i].githubUrl}">GitHub Link</a></p>
+            <div class="projectLinksDiv">
+              <a class="jsLinks" href="${projects[i].url}">${projects[i].title}</a><br>
+              <a class="jsLinks" href="${projects[i].githubUrl}">GitHub</a>
+            </div>
           </div>`;
         }
       }
