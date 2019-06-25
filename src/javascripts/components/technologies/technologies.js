232a356ec053cb2bb5f6ebc8d71ec5aa0048a527
JPantana <joshpantana@me.com>
2019-06-25 18:14:29 -0500

    fixed svgs not showing up in deploye

--- src/javascripts/components/technologies/technologies.js
+++ src/javascripts/components/technologies/technologies.js
@@ -1,5 +1,3 @@
-// import projectsData from '../../helpers/data/projectsData';
-
 import util from '../../helpers/util';
 
 import './technologies.scss';
@@ -14,43 +12,43 @@ const techDivBuilder = (e) => {
     <ul class="tech-container">
       <li>
         <div class="techImgDiv">
-            <img data-wow-delay=".5s" class="techImg wow bounceIn logo-width" src="imgs/html5.svg" style="visibility: visible; animation-delay: 0.5s; animation-name: pulse;">
+            <img data-wow-delay=".5s" class="techImg wow bounceIn logo-width" src="/imgs/html5.svg" style="visibility: visible; animation-delay: 0.5s; animation-name: pulse;">
             <h3 class="techH3">HTML5</h3>
         </div>
       </li>
       <li>
         <div class="techImgDiv">
-          <img data-wow-delay=".15" class="techImg wow bounceIn logo-width" src="imgs/css-5.svg" style="visibility: visible; animation-delay: .5s; animation-name: pulse;">
+          <img data-wow-delay=".15" class="techImg wow bounceIn logo-width" src="/imgs/css-5.svg" style="visibility: visible; animation-delay: .5s; animation-name: pulse;">
             <h3 class="techH3">CSS3</h3>
           </div>
       </li>
       <li>
         <div class="techImgDiv">
-          <img data-wow-delay=".15" class="techImg wow bounceIn logo-width sassIcon" src="imgs/sass-1.svg" style="visibility: visible; animation-delay: .5s; animation-name: pulse;">
+          <img data-wow-delay=".15" class="techImg wow bounceIn logo-width sassIcon" src="/imgs/sass-1.svg" style="visibility: visible; animation-delay: .5s; animation-name: pulse;">
             <h3 class="techH3">SCSS</h3>
           </div>
       </li>
       <li>
         <div class="techImgDiv">
-          <img data-wow-delay=".5s" class="javascriptIcon techImg wow bounceIn logo-width" src="imgs/javascript-1.svg" style="visibility: visible; animation-delay: 0.5s; animation-name: pulse;">
+          <img data-wow-delay=".5s" class="javascriptIcon techImg wow bounceIn logo-width" src="/imgs/javascript-1.svg" style="visibility: visible; animation-delay: 0.5s; animation-name: pulse;">
           <h3 class="techH3">JavaScript</h3>
         </div>
       </li>
       <li>
         <div class="techImgDiv">
-          <img data-wow-delay=".5s" class="reactIcon techImg wow bounceIn logo-width" src="imgs/react.svg" style="visibility: visible; animation-delay: 0.5s; animation-name: pulse;">
+          <img data-wow-delay=".5s" class="reactIcon techImg wow bounceIn logo-width" src="/imgs/react.svg" style="visibility: visible; animation-delay: 0.5s; animation-name: pulse;">
           <h3 class="techH3">React</h3>
         </div>
       </li>
       <li>
         <div class="techImgDiv">
-          <img data-wow-delay=".5s" class="bootstrapIcon techImg wow bounceIn logo-width" src="imgs/bootstrap-4.svg" style="visibility: visible; animation-delay: 0.5s; animation-name: pulse;">
+          <img data-wow-delay=".5s" class="bootstrapIcon techImg wow bounceIn logo-width" src="/imgs/bootstrap-4.svg" style="visibility: visible; animation-delay: 0.5s; animation-name: pulse;">
           <h3 class="techH3">Bootstrap</h3>
         </div>
       </li>
       <li>
         <div class="techImgDiv">
-          <img data-wow-delay=".5s" class="techImg wow bounceIn logo-width" src="imgs/visual-studio-code-1.svg" style="visibility: visible; animation-delay: 0.5s; animation-name: pulse;">
+          <img data-wow-delay=".5s" class="techImg wow bounceIn logo-width" src="/imgs/visual-studio-code-1.svg" style="visibility: visible; animation-delay: 0.5s; animation-name: pulse;">
           <h3 class="techH3">Visual Studio Code</h3>
         </div>
       </li>
